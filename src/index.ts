4f5e2337c89cd5b9125e224255514e26164d2fb5
Oleg Valter <o.a.philippov@gmail.com>
2021-05-27 08:13:57 +0300

    Added main module

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import { bgRed } from "chalk";
+import { writeFile } from "fs/promises";
+import * as yargs from "yargs";
+import {
+  generateTampermonkeyHeaders,
+  generateViolentMonkeyHeaders,
+  GeneratorMap,
+  UserScriptManagerName,
+} from "./generators";
+import { getPackage } from "./utils";
+
+const names: UserScriptManagerName[] = ["tampermonkey", "violentmonkey"];
+
+type ParsedArgs = Partial<{
+  output: string;
+  package: string;
+  type: UserScriptManagerName;
+}>;
+
+const cli = yargs(process.argv.slice(2));
+cli.option("o", {
+  alias: "output",
+  default: "./dist/headers.js",
+});
+cli.option("p", {
+  alias: "package",
+  default: "./package.json",
+  type: "string",
+});
+cli.option("t", {
+  alias: "type",
+  choices: names,
+  description: "choose userscript type",
+  demandOption: true,
+  type: "string",
+});
+
+export const generate = async () => {
+  const parsed = await cli.parse();
+
+  const { type, output, package: path } = <ParsedArgs>parsed;
+
+  const managerTypeMap: GeneratorMap = {
+    tampermonkey: generateTampermonkeyHeaders,
+    violentmonkey: generateViolentMonkeyHeaders,
+  };
+
+  try {
+    const parsedPackage = await getPackage(path!);
+
+    if (!parsedPackage) return console.log(bgRed`missing or corrupted package`);
+
+    const content = managerTypeMap[type!](parsedPackage);
+
+    await writeFile(output!, content, { encoding: "utf-8" });
+  } catch ({ message }) {
+    console.log(bgRed`failed to generate headers: ${message}`);
+  }
+};
+
+generate();
