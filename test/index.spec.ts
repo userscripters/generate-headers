ce400d69f51dcedfb9bfec408842ce1e142e50e2
Oleg Valter <o.a.philippov@gmail.com>
2021-06-20 21:36:15 +0300

    added tests for the -m option

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,7 +4,7 @@ import { exec } from "child_process";
 import { readFile, stat, unlink } from "fs/promises";
 import { join } from "path";
 import { promisify } from "util";
-import { generate } from "../src";
+import { generate, GeneratorOptions } from "../src";
 import { getLongest } from "../src/utils";
 
 use(cpr);
@@ -12,100 +12,152 @@ use(cpr);
 const aexec = promisify(exec);
 
 describe("main", () => {
-  const base = process.cwd();
-  const pkg = join(base, "/package.json");
-  const output = join(base, "/test/headers.js");
-  const entry = "./src/index.ts";
+    const base = process.cwd();
+    const pkg = join(base, "/package.json");
+    const output = join(base, "/test/headers.js");
+    const entry = "./src/index.ts";
 
-  const common = { output, packagePath: pkg };
+    const common: GeneratorOptions = { output, packagePath: pkg };
 
-  describe.skip("Greasemonkey", async () => {
-    //TODO: add once other commands ready
-  });
-
-  describe("CLI Options", async function () {
-    this.timeout(5e3);
-
-    afterEach(async () =>
-      stat(output)
-        .then(() => unlink(output))
-        .catch(() => {})
-    );
-
-    it("-d option should forgo file generation", async () => {
-      const { stdout } = await aexec(
-        `ts-node ${entry} tampermonkey -p ${pkg} -d`
-      );
-      expect(!!stdout).to.be.true;
-      expect(stat(output)).to.eventually.be.rejected;
+    describe.skip("Greasemonkey", async () => {
+        //TODO: add once other commands ready
     });
 
-    it("-d option should override -o", async () => {
-      await aexec(`ts-node ${entry} tampermonkey -p ${pkg} -o ${output} -d`);
-      expect(stat(output)).to.eventually.be.rejected;
+    describe("CLI Options", async function () {
+        this.timeout(5e3);
+
+        afterEach(async () =>
+            stat(output)
+                .then(() => unlink(output))
+                .catch(() => {})
+        );
+
+        it("-d option should forgo file generation", async () => {
+            const { stdout } = await aexec(
+                `ts-node ${entry} tampermonkey -p ${pkg} -d`
+            );
+            expect(!!stdout).to.be.true;
+            expect(stat(output)).to.eventually.be.rejected;
+        });
+
+        it("-d option should override -o", async () => {
+            await aexec(
+                `ts-node ${entry} tampermonkey -p ${pkg} -o ${output} -d`
+            );
+            expect(stat(output)).to.eventually.be.rejected;
+        });
+
+        it("-m options should correctly add @matches", async () => {
+            //@see https://developer.chrome.com/docs/extensions/mv2/match_patterns/
+            const matches: string[] = [
+                "http://*/*",
+                "http://*/foo*",
+                "https://*.google.com/foo*bar",
+                "http://example.org/foo/bar.html",
+                "file:///foo*",
+                "http://127.0.0.1/*",
+                "*://mail.google.com/*",
+                "urn:*",
+                "<all_urls>",
+            ];
+
+            const mOpts = matches.map((m) => `-m "${m}"`).join(" ");
+
+            const { stdout } = await aexec(
+                `ts-node ${entry} tampermonkey ${mOpts} -p ${pkg} -o ${output} -d`
+            );
+
+            const matched = stdout.match(/@matches\s+(.+)/g) || [];
+            expect(matched).length(9);
+        });
+
+        it("-s option should control number of spaces added", async () => {
+            const sp = 8;
+
+            await aexec(
+                `ts-node ${entry} tampermonkey -s ${sp} -p ${pkg} -o ${output}`
+            );
+            const contents = await readFile(output, { encoding: "utf-8" });
+
+            const lines = contents.split("\n");
+
+            const matches = lines
+                .map((line) => line.match(/(\/\/ @\w+)\s+/) || [])
+                .filter(({ length }) => length);
+
+            const headers = matches.map(([, header]) => header);
+            const headlines = matches.map(({ input }) => input);
+
+            const longest = getLongest(headers);
+
+            const [fistHeader] = headlines;
+
+            const index = fistHeader!.search(/(?<=^\/\/\s@\w+\s+)\w/);
+            expect(longest + sp + 1).to.be.equal(index);
+        });
     });
 
-    it("-s option should control number of spaces added", async () => {
-      const sp = 8;
+    describe("Tampermonkey", async () => {
+        const artefacts: string[] = [];
 
-      await aexec(
-        `ts-node ${entry} tampermonkey -s ${sp} -p ${pkg} -o ${output}`
-      );
-      const contents = await readFile(output, { encoding: "utf-8" });
+        const directCommon: GeneratorOptions = { ...common, direct: true };
 
-      const lines = contents.split("\n");
+        //make sure test output will be cleared
+        beforeEach(() => artefacts.push(output));
 
-      const matches = lines
-        .map((line) => line.match(/(\/\/ @\w+)\s+/) || [])
-        .filter(({ length }) => length);
+        afterEach(() => {
+            Promise.all(artefacts.map(unlink));
+            artefacts.length = 0;
+        });
 
-      const headers = matches.map(([, header]) => header);
-      const headlines = matches.map(({ input }) => input);
+        it("headers are generated correctly", async () => {
+            const content = await generate("tampermonkey", directCommon);
+            expect(!!content).to.be.true;
+        });
 
-      const longest = getLongest(headers);
+        it("@matches headers should be generated", async () => {
+            //@see https://developer.chrome.com/docs/extensions/mv2/match_patterns/
+            const matches: string[] = [
+                "http://*/*",
+                "http://*/foo*",
+                "https://*.google.com/foo*bar",
+                "http://example.org/foo/bar.html",
+                "file:///foo*",
+                "http://127.0.0.1/*",
+                "*://mail.google.com/*",
+                "urn:*",
+                "<all_urls>",
+            ];
 
-      const [fistHeader] = headlines;
+            const content = await generate("tampermonkey", {
+                ...directCommon,
+                matches,
+            });
 
-      const index = fistHeader!.search(/(?<=^\/\/\s@\w+\s+)\w/);
-      expect(longest + sp + 1).to.be.equal(index);
+            const matched = content.match(/@matches\s+(.+)/g) || [];
+            expect(matched).length(9);
+        });
+
+        it("header names should be equally indented", async () => {
+            const content = await generate("tampermonkey", directCommon);
+
+            const headerMatcher = /(?<=^\/\/\s@\w+\s+)\w/;
+
+            const valuePostions = content
+                .split("\n")
+                .map((line) => line.search(headerMatcher));
+
+            const onlyMatched = valuePostions.filter((line) => line !== -1);
+            const [firstIndex] = onlyMatched;
+
+            const allSameChar = onlyMatched.every(
+                (index) => index === firstIndex
+            );
+            expect(allSameChar).to.be.true;
+        });
     });
-  });
 
-  describe("Tampermonkey", async () => {
-    const artefacts: string[] = [];
-
-    afterEach(() => {
-      Promise.all(artefacts.map(unlink));
-      artefacts.length = 0;
+    describe.skip("ViolentMonkey", async () => {
+        //TODO: add once other commands ready
     });
-
-    it("headers are generated correctly", async () => {
-      artefacts.push(output);
-
-      const content = await generate("tampermonkey", common);
-      expect(!!content).to.be.true;
-    });
-
-    it("header names should be equally indented", async () => {
-      artefacts.push(output);
-
-      const content = await generate("tampermonkey", common);
-
-      const headerMatcher = /(?<=^\/\/\s@\w+\s+)\w/;
-
-      const valuePostions = content
-        .split("\n")
-        .map((line) => line.search(headerMatcher));
-
-      const onlyMatched = valuePostions.filter((line) => line !== -1);
-      const [firstIndex] = onlyMatched;
-
-      const allSameChar = onlyMatched.every((index) => index === firstIndex);
-      expect(allSameChar).to.be.true;
-    });
-  });
-
-  describe.skip("ViolentMonkey", async () => {
-    //TODO: add once other commands ready
-  });
 });
