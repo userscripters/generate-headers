6c566b01c7b9aff721fb9f4adff276ea1beb5633
Oleg Valter <o.a.philippov@gmail.com>
2021-11-28 04:02:09 +0300

    added array uniqifier utility

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,7 +1,6 @@
-export type RequiredProps<T, K extends keyof T = keyof T> = T &
-    {
-        [P in K]-?: T[P];
-    };
+export type RequiredProps<T, K extends keyof T = keyof T> = T & {
+    [P in K]-?: T[P];
+};
 
 export const getLongest = (words: string[]) =>
     Math.max(...words.map(({ length }) => length));
@@ -10,3 +9,5 @@ export const scase = (text: string) =>
     `${text[0].toUpperCase()}${text.slice(1).toLowerCase()}`;
 
 export const mdLink = (lbl: string, href: string) => `[${lbl}](${href})`;
+
+export const uniqify = <T>(arr: T[]) => [...new Set(arr)];
