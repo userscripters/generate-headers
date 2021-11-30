43ceb4e468c8f17e352e6fec902937f8e04c8de9
Oleg Valter <o.a.philippov@gmail.com>
2021-11-30 17:55:30 +0300

    parallelized CLI async exec runs to speed up tests

--- test/cli.spec.ts
+++ test/cli.spec.ts
@@ -26,10 +26,23 @@ describe("CLI Options", async function () {
             .catch(() => {})
     );
 
+    /**
+     * CLI runs are grouped before test cases to achieve some level
+     * of parallelization for CLI tests (each can take up to 5s)
+     */
+    const cliPfx = `ts-node ${entry}`;
+    const cliRuns = await Promise.all([
+        aexec(`${cliPfx} tampermonkey -p ${pkg} -d`),
+        aexec(`${cliPfx} violentmonkey -i "content" -p ${pkg} -o ${output} -d`),
+        aexec(`${cliPfx} tampermonkey -i "page" -p ${pkg} -o ${output} -d`),
+        aexec(`${cliPfx} tampermonkey -p ${pkg} -o ${output} -d`),
+        aexec(`${cliPfx} violentmonkey -p ${pkg} -o ${output} -d -g all`),
+        aexec(`${cliPfx} tampermonkey -m all -c`),
+        aexec(`${cliPfx} tampermonkey -p ${pkg} -d --pretty`),
+    ]);
+
     it("-d option should forgo file generation", async () => {
-        const { stdout } = await aexec(
-            `ts-node ${entry} tampermonkey -p ${pkg} -d`
-        );
+        const { stdout } = cliRuns[0];
         expect(!!stdout).to.be.true;
         expect(stat(output)).to.eventually.be.rejected;
     });
@@ -39,17 +52,13 @@ describe("CLI Options", async function () {
         expect(stat(output)).to.eventually.be.rejected;
     });
 
-    it("-i option should add inject-into header for Violentmonkey", async () => {
-        const { stdout } = await aexec(
-            `ts-node ${entry} violentmonkey -i "content" -p ${pkg} -o ${output} -d`
-        );
+    it("-i option should add inject-into header for Violentmonkey", () => {
+        const { stdout } = cliRuns[1];
         expect(stdout).to.match(/^\/\/ @inject-into\s+content$/gm);
     });
 
-    it("-i option should result in no-op for non-Violentmonkey managers", async () => {
-        const { stdout } = await aexec(
-            `ts-node ${entry} tampermonkey -i "page" -p ${pkg} -o ${output} -d`
-        );
+    it("-i option should result in no-op for non-Violentmonkey managers", () => {
+        const { stdout } = cliRuns[2];
         expect(stdout).to.not.match(/^\/\/ @inject-into\s+page$/gm);
     });
 
@@ -72,19 +81,14 @@ describe("CLI Options", async function () {
         expect(allAreTampermonkeyHeaders).to.be.true;
     });
 
-    it('no -g options should add @grant "none"', async () => {
-        const { stdout } = await aexec(
-            `ts-node ${entry} tampermonkey -p ${pkg} -o ${output} -d`
-        );
-
+    it('no -g options should add @grant "none"', () => {
+        const { stdout } = cliRuns[3];
         const isNone = /@grant\s+none/.test(stdout);
         expect(isNone).to.be.true;
     });
 
-    it("-g option set to 'all' should include all possible grants", async () => {
-        const { stdout } = await aexec(
-            `ts-node ${entry} violentmonkey -p ${pkg} -o ${output} -d -g all`
-        );
+    it("-g option set to 'all' should include all possible grants", () => {
+        const { stdout } = cliRuns[4];
 
         grantsVM.forEach((grant) => {
             const status = new RegExp(`^// @grant\\s+${grant}$`, "gm").test(
@@ -103,6 +107,11 @@ describe("CLI Options", async function () {
 
         const matched = stdout.match(/@match\s+(.+)/g) || [];
         expect(matched).length(allMatches.length);
+    });
+
+    it("-c option should correctly collapse -m all [template] output", async () => {
+        const { stdout } = cliRuns[5];
+        expect(stdout).to.match(/\*\.stackexchange\.com/);
     });
 
     it("-r option should correctly add @run-at", async function () {
@@ -149,10 +158,7 @@ describe("CLI Options", async function () {
     });
 
     it("--pretty option should format headers correctly", async () => {
-        const { stdout } = await aexec(
-            `ts-node ${entry} tampermonkey -p ${pkg} -d --pretty`
-        );
-
+        const { stdout } = cliRuns[6];
         const [, name] = /\/\/ @name\s+(.+)$/m.exec(stdout) || [];
         expect(name).to.be.equal("Generate Headers");
     });
