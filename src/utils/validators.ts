0810fb778543c294f786b384cb7cd27df8d133b8
Oleg Valter <o.a.philippov@gmail.com>
2022-01-13 02:17:12 +0300

    added getter for existing header positions

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,7 +1,38 @@
 import { valid } from "semver";
 import validator from "validator";
-import { PackageInfo } from "./package";
-import { RequiredOnly } from "./types";
+import { makeMonkeyTags } from "../generators/common/monkey";
+import { type PackageInfo } from "./package";
+import type { RequiredOnly } from "./types";
+
+export const getExistingHeadersPosition = async (path: string | URL) => {
+    const { createInterface } = await import("readline");
+    const { createReadStream } = await import("fs");
+
+    const filestream = createReadStream(path, { encoding: "utf-8" });
+    const readline = createInterface(filestream);
+
+    let currentLine = 0;
+    let openTagLine = -1;
+    let closeTagLine = -1;
+
+    const [openTag, closeTag] = makeMonkeyTags();
+
+    return new Promise<[number, number]>((resolve, reject) => {
+        readline.on("line", (line) => {
+            currentLine += 1;
+
+            if (line === openTag) openTagLine = currentLine;
+            if (line === closeTag) closeTagLine = currentLine;
+
+            if (openTagLine > -1 && closeTagLine > -1) {
+                readline.close();
+            }
+        });
+
+        readline.on("error", reject);
+        readline.on("close", () => resolve([openTagLine, closeTagLine]));
+    });
+};
 
 export const validateMatchHeaders = (matches: string[]) => {
     const validationRegex =
--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,9 +1,11 @@
 import { expect } from "chai";
 import { join } from "path";
+import { makeMonkeyTags } from "../src/generators/common/monkey";
 import { getPackage } from "../src/utils/package";
 import {
+    getExistingHeadersPosition,
     validateMatchHeaders,
-    validateRequiredHeaders,
+    validateRequiredHeaders
 } from "../src/utils/validators";
 
 describe("validators", () => {
@@ -73,5 +75,29 @@ describe("validators", () => {
             expect(missing).to.contain("name");
             expect(missing).to.contain("description");
         });
+    });
+
+    describe("existing headers checker", () => {
+        const [openTag, closeTag] = makeMonkeyTags();
+
+        it("should return the correct position of existing headers", async () => {
+            const { appendFile, rm } = await import("fs/promises");
+
+            const tmpfile = join("./", "headers.js");
+
+            await appendFile(
+                tmpfile,
+                `${openTag}\njust a header\n${closeTag}\n`,
+                { encoding: 'utf-8' }
+            );
+
+            const [start, end] = await getExistingHeadersPosition(tmpfile);
+
+            await rm(tmpfile);
+
+            expect(start).to.equal(1);
+            expect(end).to.equal(3);
+        });
+
     });
 });
