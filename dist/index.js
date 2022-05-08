7b1fe197caf7499e1ab7cec5962cf17c412885ab
Oleg Valter <o.a.philippov@gmail.com>
2022-05-08 04:34:20 +0300

    added descriptions to CLI options

--- dist/index.js
+++ dist/index.js
@@ -18,61 +18,74 @@ const sharedOpts = {
     c: {
         alias: "collapse",
         default: true,
+        description: "When using `match all <template>` option value, collapses all *.stackexchange.com sites into one wildcard match",
         type: "boolean",
     },
     d: {
         alias: "direct",
         default: false,
+        description: "Directs headers content to `process.stdout`",
         type: "boolean",
     },
     e: {
         alias: "eol",
         default: "\n",
+        description: "Sets the end-of-line character(s) (affects the check for existing headers)",
         type: "string",
     },
     g: {
         alias: "grant",
+        description: "generates @grant headers, can be repeated",
         type: "array",
     },
     i: {
         alias: "inject",
+        description: "Adds @inject-into header for Violentmonkey, no-op otherwise",
         type: "string",
     },
     m: {
         alias: "match",
+        description: "generates valid @match headers (repeatable)",
         type: "array",
     },
     o: {
         alias: "output",
         default: "./dist/headers.js",
+        description: "Creates and populates a file with headers content",
         type: "string",
     },
     p: {
         alias: "package",
         default: "./package.json",
+        description: "Path to package.json to extract info from",
         type: "string",
     },
     q: {
         alias: "require",
+        description: "generates valid @require headers (repeatable)",
         type: "array"
     },
     r: {
         alias: "run",
         default: "start",
+        description: "Adds @run-at header (values missing in manager are silently dropped)",
         type: "string",
     },
     s: {
         alias: "spaces",
         default: 4,
+        description: "number of spaces to indent header values with (total is the longest name + this value)",
         type: "number",
     },
     w: {
         alias: "whitelist",
+        description: "generates @connect headers (repeatable)",
         type: "array",
     },
     pretty: {
         type: "boolean",
         default: false,
+        description: "prettifies outputted headers where possible",
     },
 };
 names.forEach((name) => cli.command(name, `generates ${(0, common_1.scase)(name)} headers`, sharedOpts, ({ c, d, e, g = [], i, m = [], q = [], o, p, r = "start", s, pretty, w = [] }) => void (0, generate_1.generate)(name, {
