68d6b9d8fa869223b711b0d4916d8a7116729484
Oleg Valter <o.a.philippov@gmail.com>
2022-05-23 05:50:18 +0300

    added generateCustomHeaders utility

---
+++ src/generators/custom.ts
@@ -0,0 +1,14 @@
+import type { HeaderEntries } from "./index.js";
+
+export type CustomHeaders = { contributors: string; } & { [name: string]: string; };
+
+/**
+ * @summary generates custom headers not built-in to any of the managers
+ * @param custom list of custom headers
+ */
+export const generateCustomHeaders = (custom: string[]): HeaderEntries<CustomHeaders> => {
+    return custom.map((pair) => {
+        const [name, value = ""] = pair.split(/\s+/);
+        return [name, value];
+    });
+};
