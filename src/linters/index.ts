122846cb16161f3cff10b4e1b360ca1b3802f36e
Oleg Valter <o.a.philippov@gmail.com>
2022-06-26 02:07:17 +0300

    added unit tests for the linter module (#126)

--- src/linters/index.ts
+++ src/linters/index.ts
@@ -41,72 +41,8 @@ export const lint = async (metadataBlock: string, options: LintOptions = {}): Pr
 
     const formatter = await eslint.loadFormatter("stylish");
 
-    console.log(await formatter.format(results));
-
     return {
         error: await formatter.format(results),
         headers: fix && output ? output : metadataBlock
     };
 };
-
-(async () => {
-    const output = await lint(`// ==UserScript==
-// @author          benjol
-// @name            Auto Review Comments
-// @contributors    double beep (https://github.com/double-beep), Oleg Valter (https://github.com/Oaphi)
-// @description     No more re-typing the same comments over and over!
-// @grant           GM_deleteValue
-// @grant           GM_getValue
-// @grant           GM_listValues
-// @grant           GM_setValue
-// @homepage        https://github.com/userscripters/SE-AutoReviewComments#readme
-// @match           https://*.stackexchange.com/questions/*
-// @match           https://*.stackexchange.com/review/*
-// @match           https://askubuntu.com/questions/*
-// @match           https://askubuntu.com/review/*
-// @match           https://es.meta.stackoverflow.com/questions/*
-// @match           https://es.meta.stackoverflow.com/review/*
-// @match           https://es.stackoverflow.com/questions/*
-// @match           https://es.stackoverflow.com/review/*
-// @match           https://ja.meta.stackoverflow.com/questions/*
-// @match           https://ja.meta.stackoverflow.com/review/*
-// @match           https://ja.stackoverflow.com/questions/*
-// @match           https://ja.stackoverflow.com/review/*
-// @match           https://mathoverflow.net/questions/*
-// @match           https://mathoverflow.net/review/*
-// @match           https://meta.askubuntu.com/questions/*
-// @match           https://meta.askubuntu.com/review/*
-// @match           https://meta.mathoverflow.net/questions/*
-// @match           https://meta.mathoverflow.net/review/*
-// @match           https://meta.serverfault.com/questions/*
-// @match           https://meta.serverfault.com/review/*
-// @match           https://meta.stackoverflow.com/questions/*
-// @match           https://meta.stackoverflow.com/review/*
-// @match           https://meta.superuser.com/questions/*
-// @match           https://meta.superuser.com/review/*
-// @match           https://pt.meta.stackoverflow.com/questions/*
-// @match           https://pt.meta.stackoverflow.com/review/*
-// @match           https://pt.stackoverflow.com/questions/*
-// @match           https://pt.stackoverflow.com/review/*
-// @match           https://ru.meta.stackoverflow.com/questions/*
-// @match           https://ru.meta.stackoverflow.com/review/*
-// @match           https://ru.stackoverflow.com/questions/*
-// @match           https://ru.stackoverflow.com/review/*
-// @match           https://serverfault.com/questions/*
-// @match           https://serverfault.com/review/*
-// @match           https://stackapps.com/questions/*
-// @match           https://stackapps.com/review/*
-// @match           https://stackoverflow.com/questions/*
-// @match           https://stackoverflow.com/review/*
-// @match           https://superuser.com/questions/*
-// @match           https://superuser.com/review/*
-// @run-at          document-start
-// @source          git+https://github.com/userscripters/SE-AutoReviewComments.git
-// @supportURL      https://github.com/userscripters/SE-AutoReviewComments/issues
-// @version         1.4.7
-// ==/UserScript==
-
-`);
-
-    console.log(output);
-})();
---
+++ test/linting.spec.ts
@@ -0,0 +1,61 @@
+import { lint } from "../src/linters/index.js";
+
+import { generate } from "../src/generate.js";
+
+import { expect } from "chai";
+import { managers, output, pkg } from "./index.spec.js";
+
+describe(lint.name, () => {
+    it("should return empty 'error' and same 'output' on no errors", async () => {
+        for (const manager of managers) {
+            const input = await generate(manager, {
+                collapse: true,
+                output,
+                packagePath: pkg,
+            });
+
+            const { error, headers } = await lint(input, { spaces: 4 });
+
+            expect(error).to.be.empty;
+            expect(headers).to.equal(input);
+        }
+    });
+
+    it("should return empty 'error' and fixed 'output' on errors and 'fix' enabled", async () => {
+        const input = await generate("tampermonkey", {
+            collapse: true,
+            output,
+            packagePath: pkg,
+        });
+
+        const { error, headers } = await lint(input, { spaces: 2, fix: true });
+
+        expect(error).to.be.empty;
+        expect(headers).to.not.equal(input);
+    });
+
+    it("should return filled 'error' and same 'output' on errors and 'fix' disabled", async () => {
+        const input = await generate("tampermonkey", {
+            collapse: true,
+            output,
+            packagePath: pkg,
+        });
+
+        const { error, headers } = await lint(input, { spaces: 2 });
+
+        expect(error).to.include("userscripts/align-attributes");
+        expect(headers).to.equal(input);
+    });
+
+    it("should require homepageURL if the manager supports it", async () => {
+        const input = await generate("tampermonkey", {
+            collapse: true,
+            output,
+            packagePath: pkg,
+        });
+
+        const { error } = await lint(input, { spaces: 4, isHomepageAllowed: true });
+
+        expect(error).to.include("userscripts/use-homepage-and-url");
+    });
+});
